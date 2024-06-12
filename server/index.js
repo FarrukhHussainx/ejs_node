095c5781e3a81bdaf86e2807cc2be77063c3a266
farrukhhussainx <fa4ukhhuxxain101@gmail.com>
2024-06-12 17:04:09 +0500

    comple_

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
 import express from "express";
+const app = express();
+
+app.use(express.json());
+
+let bowl = ["apple", "orange", "pears"];
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    dayType: "a weak day",
+    advice: "its time to work hard",
+    fruits: bowl,
+  });
+});
+app.post("/submit", (req, res) => {
+  const name = req.body.namex;
+  console.log(name);
+  res.send(200);
+});
+
+app.listen(3000, () => {
+  console.log("app listen a 3000");
+});
